40768229ba09665e6ab0dd3d32e13f87cb4ca1b4
Lou <microwin2@gmail.com>
2023-09-28 20:32:55 +0200

    wip: vacarion sheets downloader

--- index.ts
+++ index.ts
@@ -1,9 +1,20 @@
+import vacationSheetDownloader from './vacation_sheet_downloader';
 import vacationSheetExtractor from './vacation_sheet_extractor';
 import path from "path";
 import fs from 'fs';
 
 console.log('germany vacations API start');
 
+/**
+ * Download current vacation sheets PDFs
+ */
+const vacDownloader = new vacationSheetDownloader();
+vacDownloader.download();
+
+
+/**
+ * Extract vaction dates from PDF files.
+ */
 const pdfDir = 'vacation_sheets';
 const vacationSheetsPath = path.join(__dirname, `../${pdfDir}`);
 
--- vacation_sheet_downloader.ts
+++ vacation_sheet_downloader.ts
@@ -1,3 +1,42 @@
+import { JSDOM } from "jsdom";
+
 class VacationSheetDownloader {
-    
+
+
+    public async download() {
+
+        const pdfs = await this.fetchPdfDownloadLinks();
+
+        console.log('PDFs for download:', pdfs);
+    }
+
+    private async fetchPdfDownloadLinks(): Promise<string[]>{
+        const pdfFilesForDownload: string[] = [];
+
+        const baseUrl = 'https://www.kmk.org/';
+        const resp = await fetch(`${baseUrl}service/ferien.html`);
+        const respText = await resp.text();
+
+        const pageDom = new JSDOM(respText);
+
+        const vacationSheetsLinkSelector = "a[href^='/fileadmin/Dateien/pdf/Ferienkalender/']";
+        const linkElements = pageDom.window.document.querySelectorAll(vacationSheetsLinkSelector);
+
+        linkElements.forEach(linkElement => {
+            const text = linkElement.textContent;
+            const link = linkElement.getAttribute('href');
+
+            if(text.startsWith('Ferientermine im Schuljahr') && link.endsWith('.pdf')){
+                pdfFilesForDownload.push(baseUrl+link);
+            }
+        });
+
+        if(pdfFilesForDownload.length === 0) console.warn('Found no pdf links for vacation sheet download!');
+
+        return pdfFilesForDownload;
+
+    }
+
 }
+
+export default VacationSheetDownloader
